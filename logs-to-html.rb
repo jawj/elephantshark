f0af70b30964310024a62be3d7724cc4bdbc804f
George MacKerron <georgemackerron@neon.tech>
2025-10-07 16:56:03 +0200

    Added HTML transformation script for logs

---
+++ logs-to-html.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# elephantshark --no-bw | ./logs-to-html.rb > ~/Downloads/eslog.html
+
+html = %{<!DOCTYPE html>
+<html><head><title>elephantshark log</title>
+  <style>
+    body { margin: 0; padding: 0; overflow-x: scroll; background: #002050; color: #eee; }
+    pre { display: inline-block; padding: 10px 20px; margin: 0; font: 14px/1.4 'IBM Plex Mono', monospace; }
+    .c33 { color: #ffee00; }
+    .c35 { color: #00dddd; }
+    .c36 { color: #ff44ff; }
+  </style>
+</head><body><pre>
+}
+
+ARGF.each do |raw|
+  line = raw.chomp
+    .gsub(/[<>"']/, { '<' => '&lt;', '>' => '&gt;', '"' => '&quot;', "'" => '&apos;' })
+    .gsub(/\033\[(\d+)m(.*?)\033\[0m/, '<span class="c\1">\2</span>')
+  html += line + "\n"
+end
+
+html += %{
+</pre></body></html>
+}
+
+puts html
